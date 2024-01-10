38d8a05873f08767b2d0b5fa090b7d36481a4521
Armaan <yadavarmaan10@gmail.com>
2024-01-10 19:04:13 +0530

    added about and source code link

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <div className="mt-[70px]">kya karoge hamare baare jaan kar?</div>;
+  return (
+    <div className="mt-[70px] flex flex-col text-3xl font-[600] px-3 gap-5">
+      <span>kya karoge hamare baare jaan kar?</span>
+      <span>
+        <Link
+          to={"https://github.com/armaan-yadav/Swiggy-Clone"}
+          target="_blank"
+        >
+          Click here for Source Code
+        </Link>
+      </span>
+    </div>
+  );
 };
 
 export default About;
